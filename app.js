ec9262c78442e06be199dab820b58ef37bd0f526
sebastian8080 <sebas25211@hotmail.com>
2019-07-16 19:39:14 -0500

    He agregado un gitignore

---
+++ app.js
@@ -0,0 +1,23 @@
+console.log("Hola Mundo");
+/*cuando damos el comando git init es para decirle que voy a empezar
+a utilizar sus versiones, se crea una carpeta .git, se encarga de administrar 
+todos los cambios del codigo, NO TOCAR NADA DE ESA CARPETA
+
+git status: para ver que archivos estamos trabajando, y nos recomienda usar git add <file>
+para agrear un archivo de trabajo
+
+Para configurar quien hace los cambios en el proyecto
+git config --global user.email "sebas25211@hotmail.com"
+git config --global user.name "sebastian8080"
+
+Git Log: me da un hash (cc9d56a637139eb0e269f74f3c9fa7b69c976113) que es una
+manera de diferenciar cada commit, cada snapshot o cada version de mi programa
+Ahi me dice el autor, el usuario, etc
+
+git checkout -- nombre_de_archivo: para volver al paso
+anterior de un archivo, es como un rollback
+
+git diff: me dice que fue lo que modifique
+
+HEAD -> master: es el codigo en el que estamos
+*/
